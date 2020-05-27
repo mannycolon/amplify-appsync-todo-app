2ab76923a707118fd85d5125bffd4ece2538f382
mannycolon <colonmanuel7@gmail.com>
2020-05-27 14:31:55 -0400

    Created react-app & Amplify init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
